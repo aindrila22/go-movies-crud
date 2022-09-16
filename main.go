2096cea1def42dda9590c4ac45f552757f72213a
aindrila22 <aindrilabhattacharjee72@gmail.com>
2022-09-16 12:33:39 +0530

    tested code

---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct{
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct{
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+func deleteMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r)
+	for index, item := range movies{
+		if item.ID == params["id"]{//if selected id from params matches with movie id in array
+			movies = append(movies[:index], movies[index+1:]...)//append the other movies in the array in place of the selected index
+			break//thus removing the element 
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+func getMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params := mux.Vars(r)
+	for _, item := range movies{
+		if item.ID == params["id"]{//if selected id from params matches with movie id in array
+			json.NewEncoder(w).Encode(item)//return the item
+			return
+		}
+	}
+}
+func createMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+//as this is without database the id is getting changed with update.... this is just for example
+func updateMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")//set json content
+	params := mux.Vars(r)//get id from params
+	for index, item := range movies{//for loop through the movies
+		if item.ID == params["id"]{//if selected id from params matches with movie id in array
+			movies = append(movies[:index], movies[index+1:]...)//return the item
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = strconv.Itoa(rand.Intn(100000000))
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
+
+
+func main(){
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID:"1",Isbn: "43277", Title: "Movie 1", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID:"2",Isbn: "43278", Title: "Movie 2", Director: &Director{Firstname: "Mark", Lastname: "Li"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at localhost 5000\n")
+	log.Fatal(http.ListenAndServe(":5000", r))
+}
